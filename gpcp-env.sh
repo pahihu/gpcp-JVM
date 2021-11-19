1dd046ccfca59e66ed24b669f1933fe35424cb9c
pahihu <pahihu@gmail.com>
2021-11-19 18:25:44 +0100

    added example env setup script gpcp-env.sh

---
+++ gpcp-env.sh
@@ -0,0 +1,5 @@
+# gpcp-JVM 1.4.07
+# export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+export JROOT=/opt/gpcp-JVM
+export PATH=$JROOT/bin:$PATH
+export CPSYM=.:$JROOT/symfiles:$JROOT/symfiles/JvmSystem
